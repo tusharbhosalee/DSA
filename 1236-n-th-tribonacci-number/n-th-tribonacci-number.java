92f0eb85c151edbef2cd98462790e5053c66201e
Tushar Bhosale <tusharbhosalee@gmail.com>
2026-03-11 22:55:07 +0530

    Time: 0 ms (100.00%) | Memory: 42.1 MB (68.21%) - LeetSync

---
+++ 1236-n-th-tribonacci-number/n-th-tribonacci-number.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int tribonacci(int n) {
+        if (n == 0) {
+            return 0;
+        } else if ( n == 1 || n == 2) {
+            return 1;
+        } else {
+            int first = 0;
+            int second = 1;
+            int third = 1;
+
+            for (int i = 3; i <= n; i++) {
+                int fourth = first +second + third;
+
+                first = second;
+                second = third;
+                third = fourth;
+            }
+
+            return third;
+
+        }
+    }
+}

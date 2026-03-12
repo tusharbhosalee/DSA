0f2ce7a172fc1f7870c8b5d2a1abd8eccb75fabd
Tushar Bhosale <tusharbhosalee@gmail.com>
2026-03-12 15:26:56 +0530

    Time: 45 ms (27.06%) | Memory: 47.3 MB (19.90%) - LeetSync

--- 1-two-sum/two-sum.java
+++ 1-two-sum/two-sum.java
@@ -7,7 +7,7 @@ class Solution {
                 if (nums[i] + nums[j] == target) {
                     answer[0] = i;
                     answer[1] = j;
-                    break;
+                    return answer;
                 }
             }
         }
